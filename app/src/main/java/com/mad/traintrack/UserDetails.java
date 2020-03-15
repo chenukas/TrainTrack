8d58d8ad02419af08882136627d1c3e8c4103ee1
Chenuka Sumanasekara <58046625+chenukas@users.noreply.github.com>
2020-03-16 02:38:48 +0530

    Remade user details page since it couldn't be able to merge to base branch

---
+++ app/src/main/java/com/mad/traintrack/UserDetails.java
@@ -0,0 +1,14 @@
+package com.mad.traintrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details);
+    }
+}
