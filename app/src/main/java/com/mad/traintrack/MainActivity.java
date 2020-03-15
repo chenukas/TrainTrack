211e98cad2505e4cd7bd6327c928bc819760d0d6
harinditha <harinditharuchirawya@yahoo.com>
2020-03-15 16:23:54 +0530

    Added user Registration page

--- app/src/main/java/com/mad/traintrack/MainActivity.java
+++ app/src/main/java/com/mad/traintrack/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public Button BtnMve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +26,21 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, 3000);
+
+        BtnMve = findViewById(R.id.registration);
+
+        BtnMve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                moveToRegistration();
+
+            }
+        });
+    }
+
+    private void moveToRegistration(){
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        startActivity(intent);
     }
 }
