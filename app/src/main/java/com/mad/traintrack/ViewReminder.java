1d745e857d569edf3ae10000652e936be5cfd64d
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2020-03-16 14:48:35 +0530

    Modified reminder interfaces

--- app/src/main/java/com/mad/traintrack/ViewReminder.java
+++ app/src/main/java/com/mad/traintrack/ViewReminder.java
@@ -9,6 +9,7 @@ public class ViewReminder extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_view_reminder);
     }
 }
