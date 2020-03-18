69ff24f860d03436e7d1697d8146535c80419b47
Chenuka Sumanasekara <58046625+chenukas@users.noreply.github.com>
2020-03-18 15:04:30 +0530

    Did some common changes and some edits for interfaces

--- app/src/main/java/com/mad/traintrack/Home.java
+++ app/src/main/java/com/mad/traintrack/Home.java
@@ -9,7 +9,6 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
     }
 }
--- app/src/main/java/com/mad/traintrack/Register.java
+++ app/src/main/java/com/mad/traintrack/Register.java
@@ -9,7 +9,6 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_form);
     }
 }
