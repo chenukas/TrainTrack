27f261d86382a29fa3c4fec09f7fa3b95b53f2c4
Chenuka Sumanasekara <58046625+chenukas@users.noreply.github.com>
2020-03-19 02:08:29 +0530

    Added ticket activities

---
+++ app/src/main/java/com/mad/traintrack/PurchasedTickets.java
@@ -0,0 +1,14 @@
+package com.mad.traintrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PurchasedTickets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchased_tickets);
+    }
+}
