d1d53ae4f699b8da20397b47fa2f5c349511d58a
harinditha <harinditharuchirawya@yahoo.com>
2020-04-11 19:54:23 +0530

    Intents Created

--- app/src/main/java/com/mad/traintrack/Login.java
+++ app/src/main/java/com/mad/traintrack/Login.java
@@ -2,8 +2,10 @@ package com.mad.traintrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class Login extends AppCompatActivity {
@@ -13,5 +15,18 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        openActivityForm();
+
+    }
+
+    public void openActivityForm() {
+        Button btn = (Button) findViewById( R.id.signup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
