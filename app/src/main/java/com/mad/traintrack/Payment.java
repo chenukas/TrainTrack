7aa17af6a621fecb0663b284cbc337de90f58281
Chenuka Sumanasekara <58046625+chenukas@users.noreply.github.com>
2020-04-24 00:05:23 +0530

    Passed ticketId & total to Payment activity

--- app/src/main/java/com/mad/traintrack/Payment.java
+++ app/src/main/java/com/mad/traintrack/Payment.java
@@ -2,13 +2,25 @@ package com.mad.traintrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Payment extends AppCompatActivity {
+
+    String purchasedTicketId;
+    double total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        Intent intent = getIntent();
+        purchasedTicketId = intent.getStringExtra("ticketId");
+        total = intent.getDoubleExtra("total",0);
+
+        Toast.makeText(getApplicationContext(), "Ticket ID: " +purchasedTicketId + "\nTotal: "
+                + Double.valueOf(total).toString(),Toast.LENGTH_SHORT).show();
     }
 }
